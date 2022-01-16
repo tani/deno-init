efabc3a5e4dacf42cc27c0b5a4c0dc0eae90ba4e
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-01-16 14:19:40 +0900

    Use dynamic import

--- init.ts
+++ init.ts
@@ -1,24 +1,23 @@
 function generate(schema: any): any {
-    if ("default" in schema) {
-      return schema.default;
-    }
-    if ("type" in schema) {
-      switch (schema.type) {
-        case "object":
-          return Object.fromEntries(
-            Object.entries(schema.properties).map(([key, value]) => {
-              return [key, generate(value)];
-            }),
-          );
-        case "array":
-          return [];
-      }
+  if ("default" in schema) {
+    return schema.default;
+  }
+  if ("type" in schema) {
+    switch (schema.type) {
+      case "object":
+        return Object.fromEntries(
+          Object.entries(schema.properties).map(([key, value]) => {
+            return [key, generate(value)];
+          }),
+        );
+      case "array":
+        return [];
     }
   }
+}
 
-  const schemaUrl =
-    `https://deno.land/x/deno@v${Deno.version.deno}/cli/schemas/config-file.v1.json`;
-  const response = await fetch(schemaUrl);
-  const schema = await response.json();
-  const configFile = "deno.json";
-  Deno.writeTextFile(configFile, JSON.stringify(generate(schema), null, 2));
+const schemaUrl =
+  `https://deno.land/x/deno@v${Deno.version.deno}/cli/schemas/config-file.v1.json`;
+const { default: schema } = await import(schemaUrl, { assert: { type: "json" } });
+const configFile = "deno.json"
+Deno.writeTextFile(configFile, JSON.stringify(generate(schema), null, 2));
